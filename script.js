555cca415905c653ffe93fd0d0c108c83b443dd2
giriprasath501 <giriprasath751@gmail.com>
2025-08-12 21:04:02 +0530

    library done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const my_library = [];
 
 function book(title,author,pages,read){
-    this.id = crypto.randomUUID();
+    this.id = crypto.randomUUID().slice(0,5);
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -18,24 +18,29 @@ function addtolibrary(title,author,pages,read){
 
 }
 function displaylibrary(){
-    const libdiv = document.querySelector(".library");
+    const libdiv = document.querySelector(".show tbody");
     libdiv.innerHTML = '';
     my_library.forEach((book,index) => {
-        const bookdiv = document.createElement('div');
+        const bookdiv = document.createElement('tr');
         bookdiv.classList.add("book");
 
-        bookdiv.innerHTML = `<h3>${book.title}</h3>
-            <p>id: ${book.id}</p>
-            <p>Author: ${book.author}</p>
-            <p>Pages: ${book.pages}</p>
-            <p>Status: ${book.read ? "Read" : "Not Read"}</p>
-            <button onclick="removebook(${index})">Remove</button>`;
+        bookdiv.innerHTML = `<td>${book.id}</td>
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.pages}</td>
+            <td><button onclick="toggleReadStatus(${index})">${book.read ? "Read" : "Not Read"}</button></td>
+            <td><button onclick="removebook(${index})">Remove</button></td>`;
 
         libdiv.appendChild(bookdiv);
     });
+    document.querySelector
 }
 function removebook(index){
     my_library.splice(index,1);
+    displaylibrary();
+}
+function toggleReadStatus(index){
+    my_library[index].toggleRead();
     displaylibrary();
 }
 document.querySelector(".addbook").addEventListener("submit",function(e){
@@ -44,7 +49,7 @@ document.querySelector(".addbook").addEventListener("submit",function(e){
     const title = document.querySelector(".title").value;
     const author = document.querySelector(".author").value;
     const pages = document.querySelector(".pages").value;
-    const read = document.querySelector(".read").value;
+    const read = document.querySelector(".read").checked;
     
     addtolibrary(title,author,pages,read);
     displaylibrary();
